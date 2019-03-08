48f63c9863fbfe8055114a9a532fbef78f808337
Higgs <higgs@weidong.local>
2019-03-08 17:54:40 +0800

    commit

--- chat/train.py
+++ chat/train.py
@@ -94,10 +94,18 @@ def train():
                         eval_val, train_val = sess._tf_sess().run([predictions, train_predictions], feed_dict)
                         print('question:\t', ''.join(tokenizer.convert_ids_to_tokens(trainDatas['x'][0])))
                         print('groud truth:\t', ''.join(tokenizer.convert_ids_to_tokens(trainDatas['y'][0])))
-                        print('train predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(train_val[0])))
-                        print('train predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(train_val[1])))
-                        print('train predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(train_val[2])))
-                        print('train predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(train_val[3])))
+                        v1 = train_val[0]
+                        v1[v1<0] = 100
+                        v2 = train_val[1]
+                        v2[v2 < 0] = 100
+                        v3 = train_val[2]
+                        v3[v3 < 0] = 100
+                        v4 = train_val[3]
+                        v4[v4 < 0] = 100
+                        print('train predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(v1)))
+                        print('train predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(v2)))
+                        print('train predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(v3)))
+                        print('train predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(v4)))
                         print(train_val)
                         print(train_val.shape)
                         print('predictions:\t', ''.join(tokenizer.convert_ids_to_tokens(eval_val[0])))
